0c632e586e030f7e513d68193635cf2d9162798a
Forester Vosburgh <fvosburgh@primerevenue.com>
2015-05-15 15:42:05 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubberduck::Application.config.secret_token = '8b81dc8e9e7d6aada2032d478fe44b47723d57bea75592abfc6958d9b7b3bcec4392fe14637e2f70066175fcb35ec78457c2c55cfd4bc3387fcc1b0795f6ed26'
